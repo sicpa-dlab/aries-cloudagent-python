f728e1ad63294882db5f25d0b59e2baf8d71d70c
Adam Burdett <burdettadam@gmail.com>
2020-10-12 17:31:26 -0600

    some empty failing tests, updated message_types, more documentation
    Signed-off-by: Adam Burdett <burdettadam@gmail.com>

--- aries_cloudagent/protocols/coordinate_mediation/v1_0/handlers/tests/test_mediation_request_handler.py
+++ aries_cloudagent/protocols/coordinate_mediation/v1_0/handlers/tests/test_mediation_request_handler.py
@@ -9,12 +9,31 @@ from ......messaging.responder import MockResponder
 #from ...messages.mediate_grant import MediationGrant
 
 """
+    Tests for Mediation based on "0211: Mediator Coordination Protocol" aries-rfc. 
 """
 
 
 class TestMediationRequestHandler:
     @pytest.mark.asyncio
-    async def test_mediation_request(self):
+    async def test_for_denied_mediation_request(self):
+        """Test for MEDIATE_REQUEST request that results in MEDIATE_DENY response."""
         ctx = RequestContext()
         
-        assert True
+        assert False
+    
+    async def test_for_granted_mediation_request(self):
+        """Test for MEDIATE_REQUEST request that results in MEDIATE_GRANT response."""
+        assert False
+
+    async def test_for_list_keys_request(self):
+        """Test for KEYLIST_QUERY request that results in KEYLIST response."""
+        assert False
+
+    async def test_for_updated_key_request(self):
+        """Test for KEYLIST_UPDATE request that results in "
+            "KEYLIST_UPDATE_RESPONSE response."""
+        assert False
+    
+    async def test_for_updated_key_request(self):
+        """Test for KEYLIST_QUERY request that results in KEYLIST response."""
+        assert False
