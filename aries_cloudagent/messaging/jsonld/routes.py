c239009eda171f440797eebb99a515a290d527dc
Adam Burdett <burdettadam@gmail.com>
2021-04-08 16:35:46 -0600

    jsonld admin api update
    updated admin api to use verification method instead of verkey as well as to call new jsonld methods with session instead of wallet.
    Included in the commit is supporting decorator based tests. outstanding questions around verification method resolvings need to be addressed for next fixes.
    
    Signed-off-by: Adam Burdett <burdettadam@gmail.com>

--- aries_cloudagent/messaging/jsonld/routes.py
+++ aries_cloudagent/messaging/jsonld/routes.py
@@ -2,27 +2,26 @@
 
 from aiohttp import web
 from aiohttp_apispec import docs, request_schema, response_schema
-
+from ...admin.request_context import AdminRequestContext
+from ...messaging.models.openapi import OpenAPISchema
 from marshmallow import fields
 
-from ...admin.request_context import AdminRequestContext
-from ...wallet.base import BaseWallet
-
-from ..models.openapi import OpenAPISchema
+from ...config.base import InjectionError
+from ...wallet.error import WalletError
+from ...resolver.did_resolver import DIDResolver
+from ...resolver.base import ResolverError, BaseError
+from pydid import DIDError
 
 from .credential import sign_credential, verify_credential
-from .error import (
-    BadJWSHeaderError,
-    DroppedAttributeError,
-    MissingVerificationMethodError,
-)
 
 
 class SignRequestSchema(OpenAPISchema):
     """Request schema for signing a jsonld doc."""
 
-    verkey = fields.Str(required=True, description="verkey to use for signing")
-    doc = fields.Dict(required=True, description="JSON-LD Doc to sign")
+    verification_method = fields.Str(
+        required=True, data_key="verificationMethod", description="key"
+    )
+    document = fields.Dict(required=True, description="JSON-LD Doc to sign")
 
 
 class SignResponseSchema(OpenAPISchema):
@@ -42,28 +41,28 @@ async def sign(request: web.BaseRequest):
         request: aiohttp request object
 
     """
-    response = {}
+    context: AdminRequestContext = request["context"]
+    session = await context.session()
+    profile = context.profile
+    body = await request.json()
+    ver_meth = body.get("verificationMethod")
+    doc = body.get("document")
     try:
-        context: AdminRequestContext = request["context"]
-        body = await request.json()
-        verkey = body.get("verkey")
-        doc = body.get("doc")
-        credential = doc["credential"]
-        signature_options = doc["options"]
-
-        async with context.session() as session:
-            wallet = session.inject(BaseWallet, required=False)
-            if not wallet:
-                raise web.HTTPForbidden(reason="No wallet available")
-            document_with_proof = await sign_credential(
-                credential, signature_options, verkey, wallet
-            )
-
-        response["signed_doc"] = document_with_proof
-    except (DroppedAttributeError, MissingVerificationMethodError) as err:
-        response["error"] = str(err)
-
-    return web.json_response(response)
+        resolver = session.inject(DIDResolver)
+        ver_meth_expanded = await resolver.dereference(profile, ver_meth)
+        if ver_meth_expanded is None:
+            raise ResolverError(f"Verification method {ver_meth} not found.")
+        verkey = ver_meth_expanded.material
+        doc_with_proof = await sign_credential(
+            session, doc, {"verificationMethod": ver_meth}, verkey
+        )
+    except (DIDError, ResolverError, WalletError, InjectionError) as err:
+        if isinstance(err, BaseError):
+            raise web.HTTPBadRequest(reason=err.roll_up) from err
+        else:
+            raise web.HTTPBadRequest(
+                reason=f"internal raised error: '{type(err)}'")from err
+    return web.json_response({"signed_doc": doc_with_proof})
 
 
 class VerifyRequestSchema(OpenAPISchema):
@@ -90,28 +89,44 @@ async def verify(request: web.BaseRequest):
         request: aiohttp request object
 
     """
-    response = {"valid": False}
+    context: AdminRequestContext = request["context"]
+    profile = context.profile
+    session = await context.session()
+    body = await request.json()
+    ver_meth = body.get("verificationMethod")
+    doc = body.get("document")
     try:
-        context: AdminRequestContext = request["context"]
-        body = await request.json()
-        verkey = body.get("verkey")
-        doc = body.get("doc")
-
-        async with context.session() as session:
-            wallet = session.inject(BaseWallet, required=False)
-            if not wallet:
-                raise web.HTTPForbidden(reason="No wallet available")
-            valid = await verify_credential(doc, verkey, wallet)
-
-        response["valid"] = valid
-    except (BadJWSHeaderError, DroppedAttributeError) as e:
-        response["error"] = str(e)
-
-    return web.json_response(response)
+        resolver = session.inject(DIDResolver)
+        ver_meth_expanded = await resolver.dereference(profile, ver_meth)
+        if ver_meth_expanded is None:
+            raise ResolverError(f"Verification method {ver_meth} not found.")
+        verkey = ver_meth_expanded.material
+        result = await verify_credential(session, doc, verkey)
+    except (DIDError, ResolverError, WalletError, InjectionError) as err:
+        if isinstance(err, BaseError):
+            raise web.HTTPBadRequest(reason=err.roll_up) from err
+        else:
+            raise web.HTTPBadRequest(
+                reason=f"internal raised error: '{type(err)}'")from err
+    return web.json_response({"valid": result})
 
 
 async def register(app: web.Application):
     """Register routes."""
 
-    app.add_routes([web.post("/jsonld/sign", sign)])
-    app.add_routes([web.post("/jsonld/verify", verify)])
+    app.add_routes([web.post("/jsonld/sign", sign), web.post("/jsonld/verify", verify)])
+
+
+def post_process_routes(app: web.Application):
+    """Amend swagger API."""
+
+    # Add top-level tags description
+    if "tags" not in app._state["swagger_dict"]:
+        app._state["swagger_dict"]["tags"] = []
+    app._state["swagger_dict"]["tags"].append(
+        {
+            "name": "json-ld sign/verify",
+            "description": "sign and verify json-ld data.",
+            "externalDocs": {"description": "Specification"},  # , "url": SPEC_URI},
+        }
+    )
--- aries_cloudagent/messaging/jsonld/tests/test_routes.py
+++ aries_cloudagent/messaging/jsonld/tests/test_routes.py
@@ -1,19 +1,203 @@
-import json
-
-from copy import deepcopy
-
+import pytest
+import unittest
+import asyncio
+import pytest
 from asynctest import mock as async_mock, TestCase as AsyncTestCase
+from ....messaging.models.base import BaseModelError
+from ....wallet.error import WalletError
+from ....config.base import InjectionError
+from ....storage.error import StorageError, StorageNotFoundError
+import json
+from copy import deepcopy
 from pyld import jsonld
-
+from .. import credential
 from ....admin.request_context import AdminRequestContext
 from ....wallet.base import BaseWallet
-
 from .. import routes as test_module
+from ....resolver.did_resolver import DIDResolver
+from ....resolver.did_resolver_registry import DIDResolverRegistry
+from ....resolver.base import DIDNotFound, DIDMethodNotSupported
+from ....resolver.tests import DOC
+from pydid import DIDDocument
+
+did_doc = DIDDocument.deserialize(DOC)
+
+
+@pytest.fixture
+def mock_resolver():
+    did_resolver = async_mock.MagicMock()
+    did_resolver.resolve = async_mock.CoroutineMock(return_value=did_doc)
+    url = "did:example:1234abcd#4"
+    did_resolver.dereference = async_mock.CoroutineMock(
+        return_value=did_doc.dereference(url)
+    )
+    yield did_resolver
+
+
+@pytest.fixture
+def mock_sign_credential():
+    temp = test_module.sign_credential
+    sign_credential = async_mock.CoroutineMock(return_value="fake_signage")
+    test_module.sign_credential = sign_credential
+    yield test_module.sign_credential
+    test_module.sign_credential = temp
+
+
+@pytest.fixture
+def mock_verify_credential():
+    temp = test_module.verify_credential
+    verify_credential = async_mock.CoroutineMock(return_value="fake_verify")
+    test_module.verify_credential = verify_credential
+    yield test_module.verify_credential
+    test_module.verify_credential = temp
+
+
+@pytest.fixture
+def mock_sign_request(mock_sign_credential, mock_resolver):
+    context = AdminRequestContext.test_context({DIDResolver: mock_resolver})
+    outbound_message_router = async_mock.CoroutineMock()
+    request_dict = {
+        "context": context,
+        "outbound_message_router": outbound_message_router,
+    }
+    request = async_mock.MagicMock(
+        match_info={},
+        query={},
+        json=async_mock.CoroutineMock(
+            return_value={
+                "verkey": "fake_verkey",
+                "doc": {},
+                "options": {
+                    "type": "Ed25519Signature2018",
+                    "created": "2020-04-10T21:35:35Z",
+                    "verificationMethod": (
+                        "did:key:"
+                        "z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd#"
+                        "z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd"
+                    ),
+                    "proofPurpose": "assertionMethod",
+                },
+            },
+        ),
+        __getitem__=lambda _, k: request_dict[k],
+    )
+    yield request
+
+
+@pytest.fixture
+def mock_verify_request(mock_verify_credential, mock_resolver):
+    context = AdminRequestContext.test_context({DIDResolver: mock_resolver})
+    outbound_message_router = async_mock.CoroutineMock()
+    request_dict = {
+        "context": context,
+        "outbound_message_router": outbound_message_router,
+    }
+    request = async_mock.MagicMock(
+        match_info={},
+        query={},
+        json=async_mock.CoroutineMock(
+            return_value={
+                "doc": {
+                    "@context": "https://www.w3.org/2018/credentials/v1",
+                    "type": "VerifiablePresentation",
+                    "holder": "did:key:z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd",
+                    "proof": {
+                        "type": "Ed25519Signature2018",
+                        "created": "2021-02-16T15:21:38.512Z",
+                        "challenge": "5103d61a-bd26-4b1a-ab62-87a2a71281d3",
+                        "domain": "svip-issuer.ocs-support.com",
+                        "jws": "eyJhbGciOiJFZERTQSIsImI2NCI6ZmFsc2UsImNyaXQiOlsiYjY0Il19..mH_j_Y7MUIu_KXU_1Dy1BjE4w52INieSPaN7FPtKQKZYTRydPYO5jbjeM-uWB5BXpxS9o-obI5Ztx5IXex-9Aw",
+                        "proofPurpose": "authentication",
+                        "verificationMethod": "did:key:z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd#z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd",
+                    },
+                }
+            }
+        ),
+        __getitem__=lambda _, k: request_dict[k],
+    )
+    yield request
+
+
+@pytest.fixture
+def mock_response():
+    json_response = async_mock.MagicMock()
+    temp_value = test_module.web.json_response
+    test_module.web.json_response = json_response
+    yield json_response
+    test_module.web.json_response = temp_value
+
+
+@pytest.mark.asyncio
+async def test_sign(mock_sign_request, mock_response):
+    await test_module.sign(mock_sign_request)
+    mock_response.assert_called_once_with({"signed_doc": "fake_signage"})
+
+
+@pytest.mark.parametrize(
+    "error", [DIDNotFound, DIDMethodNotSupported, WalletError, InjectionError]
+)
+@pytest.mark.asyncio
+async def test_sign_bad_req_error(mock_sign_request, mock_response, error):
+    test_module.sign_credential = async_mock.CoroutineMock(side_effect=error())
+    with pytest.raises(test_module.web.HTTPBadRequest):
+        await test_module.sign(mock_sign_request)
+
+
+@pytest.mark.asyncio
+async def test_sign_bad_ver_meth_deref_req_error(
+    mock_resolver, mock_sign_request, mock_response
+):
+    mock_resolver.dereference.return_value = None
+    with pytest.raises(test_module.web.HTTPBadRequest):
+        await test_module.sign(mock_sign_request)
+
+
+@pytest.mark.asyncio
+async def test_verify(mock_verify_request, mock_response):
+    await test_module.verify(mock_verify_request)
+    mock_response.assert_called_once_with({"valid": "fake_verify"})
+
+
+@pytest.mark.parametrize(
+    "error", [DIDNotFound, DIDMethodNotSupported, WalletError, InjectionError]
+)
+@pytest.mark.asyncio
+async def test_verify_bad_req_error(mock_verify_request, mock_response, error):
+    test_module.verify_credential = async_mock.CoroutineMock(side_effect=error())
+    with pytest.raises(test_module.web.HTTPBadRequest):
+        await test_module.verify(mock_verify_request)
+
+
+@pytest.mark.asyncio
+async def test_verify_bad_ver_meth_deref_req_error(
+    mock_resolver, mock_verify_request, mock_response
+):
+    mock_resolver.dereference.return_value = None
+    with pytest.raises(test_module.web.HTTPBadRequest):
+        await test_module.verify(mock_verify_request)
+
+
+@pytest.mark.asyncio
+async def test_register():
+    mock_app = async_mock.MagicMock()
+    mock_app.add_routes = async_mock.MagicMock()
+    await test_module.register(mock_app)
+    mock_app.add_routes.assert_called_once()
+
+
+def test_post_process_routes():
+    mock_app = async_mock.MagicMock(_state={"swagger_dict": {}})
+    test_module.post_process_routes(mock_app)
+    assert "tags" in mock_app._state["swagger_dict"]
 
 
 class TestJSONLDRoutes(AsyncTestCase):
     async def setUp(self):
         self.context = AdminRequestContext.test_context()
+        self.ledger = async_mock.MagicMock()
+        self.registery = DIDResolverRegistry()
+        self.resolver = DIDResolver(self.registery)
+        self.context.injector.bind_instance(DIDResolver, self.resolver)
         self.did_info = await (await self.context.session()).wallet.create_local_did()
         self.request_dict = {
             "context": self.context,
@@ -28,9 +212,9 @@ class TestJSONLDRoutes(AsyncTestCase):
 
     async def test_verify_credential(self):
         POSTED_REQUEST = {  # posted json
-            "verkey": (
+            "verificationMethod": (
                 # pulled from the did:key in example
-                "5yKdnU7ToTjAoRNDzfuzVTfWBH38qyhE1b9xh4v8JaWF"
+                "did:indy:5yKdnU7ToTjAoRNDzfuzVTfWBH38qyhE1b9xh4v8JaWF#2"
             ),
             "doc": {
                 "@context": [
@@ -71,7 +255,6 @@ class TestJSONLDRoutes(AsyncTestCase):
         }
 
         self.request.json = async_mock.CoroutineMock(return_value=POSTED_REQUEST)
-
         with async_mock.patch.object(test_module.web, "json_response") as mock_response:
             result = await test_module.verify(self.request)
             assert result == mock_response.return_value
@@ -162,7 +345,7 @@ class TestJSONLDRoutes(AsyncTestCase):
 
     async def test_sign_credential(self):
         POSTED_REQUEST = {  # posted json
-            "verkey": self.did_info.verkey,
+            "verificationMethod": self.did_info.verkey,
             "doc": {
                 "credential": {
                     "@context": [
@@ -207,7 +390,48 @@ class TestJSONLDRoutes(AsyncTestCase):
             },
         }
         self.request.json = async_mock.CoroutineMock(return_value=POSTED_REQUEST)
-
+        self.ledger.resolve.return_value.set_result({
+                "credential": {
+                    "@context": [
+                        "https://www.w3.org/2018/credentials/v1",
+                        "https://www.w3.org/2018/credentials/examples/v1",
+                    ],
+                    "id": "http://example.gov/credentials/3732",
+                    "type": [
+                        "VerifiableCredential",
+                        "UniversityDegreeCredential",
+                    ],
+                    "issuer": (
+                        "did:key:" "z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd"
+                    ),
+                    "issuanceDate": "2020-03-10T04:24:12.164Z",
+                    "credentialSubject": {
+                        "id": (
+                            "did:key:"
+                            "z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd"
+                        ),
+                        "degree": {
+                            "type": "BachelorDegree",
+                            "name": u"Bachelor of Encyclopædic Arts",
+                        },
+                    },
+                },
+                "options": {
+                    # "type": "Ed25519Signature2018",  exercise default
+                    # "created": exercise default of now
+                    "creator": (
+                        "did:key:"
+                        "z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd#"
+                        "z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd"
+                    ),
+                    "verificationMethod": (
+                        "did:key:"
+                        "z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd#"
+                        "z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd"
+                    ),
+                    "proofPurpose": "assertionMethod",
+            },
+        })
         with async_mock.patch.object(test_module.web, "json_response") as mock_response:
             result = await test_module.sign(self.request)
             assert result == mock_response.return_value
@@ -252,4 +476,4 @@ class TestJSONLDRoutes(AsyncTestCase):
         mock_app.add_routes = async_mock.MagicMock()
 
         await test_module.register(mock_app)
-        assert mock_app.add_routes.call_count == 2
+        mock_app.add_routes.assert_called_once()
