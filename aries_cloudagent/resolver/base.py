04deaf10f9f3fcf6666f91e62f6deeeb924764a1
Adam Burdett <burdettadam@gmail.com>
2021-05-11 14:19:07 -0600

    Dbluhms patch applied
    Signed-off-by: Adam Burdett <burdettadam@gmail.com>

--- aries_cloudagent/resolver/base.py
+++ aries_cloudagent/resolver/base.py
@@ -2,8 +2,9 @@
 
 from abc import ABC, abstractmethod
 from enum import Enum
-from typing import Sequence, Union
+from typing import Pattern, Sequence, Union
 import re
+import warnings
 
 from pydid import DID, DIDDocument
 
@@ -41,7 +42,6 @@ class BaseDIDResolver(ABC):
             type_ (Type): Type of resolver, native or non-native
         """
         self.type = type_ or ResolverType.NON_NATIVE
-        self.supported_did_regex = ""
 
     @abstractmethod
     async def setup(self, context: InjectionContext):
@@ -53,20 +53,47 @@ class BaseDIDResolver(ABC):
         return self.type == ResolverType.NATIVE
 
     @property
-    def supported_did_regex(self):
-        """Override this property with a class var or similar to use regex matching on DIDs to determine supported resolvers"""
+    def supported_methods(self) -> Sequence[str]:
+        """Return supported methods.
+
+        DEPRECATED: Use supported_did_regex instead.
+        """
+        return []
+
+    @property
+    def supported_did_regex(self) -> Pattern:
+        """Supported DID regex for matching this resolver to DIDs it can resolve.
+
+        Override this property with a class var or similar to use regex
+        matching on DIDs to determine if this resolver supports a given DID.
+        """
         raise NotImplementedError(
-            "supported_did_regex must be overriden by subclasses of BaseResolver to use default supports method"
+            "supported_did_regex must be overriden by subclasses of BaseResolver "
+            "to use default supports method"
         )
 
-    @supported_did_regex.setter
-    def supported_did_regex(self, value):
-        self.supported_did_regex = value
-
     async def supports(self, profile: Profile, did: str) -> bool:
-        """Return if this resolver supports the given method."""
-        method_pattern = re.compile(self.supported_did_regex)
-        return bool(method_pattern.match(did))
+        """Return if this resolver supports the given DID.
+
+        Override this method to determine if this resolver supports a DID based
+        on information other than just a regular expression; i.e. check a value
+        in storage, query a resolver connection record, etc.
+        """
+        try:
+            supported_did_regex = self.supported_did_regex
+        except NotImplementedError as error:
+            if not self.supported_methods:
+                raise error
+            warnings.warn(
+                'BaseResolver.supported_methods is deprecated; use supported_did_regex instead',
+                DeprecationWarning,
+            )
+
+            supported_did_regex = re.compile(
+                '^did:(?:{}):.*$'.format('|'.join(self.supported_methods))
+            )
+
+        return bool(supported_did_regex.match(did))
 
     async def resolve(self, profile: Profile, did: Union[str, DID]) -> DIDDocument:
         """Resolve a DID using this resolver."""
